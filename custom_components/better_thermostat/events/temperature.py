e53a6a5af576cad313d0605875c7ea2378618451
Martin <110391540+albummi@users.noreply.github.com>
2025-02-10 07:46:05 +0100

    Update temperature.py

--- custom_components/better_thermostat/events/temperature.py
+++ custom_components/better_thermostat/events/temperature.py
@@ -1,6 +1,14 @@
 import logging
 
-from custom_components.better_thermostat.utils.const import CONF_HOMEMATICIP
+from custom_components.better_thermostat.utils.const import (
+    CONF_HOMEMATICIP,
+    CONF_SLEEP_MODE,
+    CONF_SLEEP_TEMPERATURE,
+    CONF_SLEEP_DELAY,
+    CONF_SLEEP_DELAY_AFTER,
+    CONF_DOOR_OVERRIDE,
+    CONF_DOOR_TEMPERATURE
+)
 from ..utils.helpers import convert_to_float
 from datetime import datetime
 from homeassistant.helpers import issue_registry as ir
@@ -77,5 +85,25 @@ async def trigger_temperature_change(self, event):
         )
         self.cur_temp = _incoming_temperature
         self.last_external_sensor_change = datetime.now()
+        self.async_write_ha_state()
+        await self.control_queue_task.put(self)
+        
+        # Handle sleep mode
+        if self.sleep_mode and self.door_override:
+            if self.door_open:
+                self.target_temperature = self.door_temperature
+                _LOGGER.debug(
+                    f"better_thermostat {self.device_name}: Door is open, setting temperature to {self.door_temperature} due to door override"
+                )
+            elif self.sleep_mode:
+                self.target_temperature = self.sleep_temperature
+                _LOGGER.debug(
+                    f"better_thermostat {self.device_name}: Sleep mode active, setting temperature to {self.sleep_temperature}"
+                )
+            else:
+                self.target_temperature = self.default_target_temperature
+                _LOGGER.debug(
+                    f"better_thermostat {self.device_name}: Setting temperature to default target temperature {self.default_target_temperature}"
+                )
         self.async_write_ha_state()
         await self.control_queue_task.put(self)
