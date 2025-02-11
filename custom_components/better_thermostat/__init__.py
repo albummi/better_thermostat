35a78ae8e2c2ff2055b9f34f127875f841b9272d
Martin <110391540+albummi@users.noreply.github.com>
2025-02-11 14:49:49 +0100

    Update __init__.py

--- custom_components/better_thermostat/__init__.py
+++ custom_components/better_thermostat/__init__.py
@@ -41,7 +41,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {}
     
-    # Plattformen vor dem Start des Ereignisloops importieren
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
     return True
