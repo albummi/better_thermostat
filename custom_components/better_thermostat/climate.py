f497cda73fb619972290cee2112571c5caae5f8d
Martin <110391540+albummi@users.noreply.github.com>
2025-02-26 08:11:03 +0100

    Update climate.py

--- custom_components/better_thermostat/climate.py
+++ custom_components/better_thermostat/climate.py
@@ -190,12 +190,12 @@ async def async_setup_entry(hass, entry, async_add_devices):
                 entry.data.get(CONF_COOLER, None),
                 hass.config.units.temperature_unit,
                 entry.entry_id,
-                device_class="better_thermostat",
-                state_class="better_thermostat_state",
                 entry.data.get(CONF_SLEEP_MODE, None),
                 entry.data.get(CONF_SLEEP_TEMPERATURE, None),
                 entry.data.get(CONF_POST_SLEEP_MODE_ACTION, None),
                 entry.data.get(CONF_POST_SLEEP_TEMPERATURE, None),
+                device_class="better_thermostat",
+                state_class="better_thermostat_state",
             )
         ]
     )
